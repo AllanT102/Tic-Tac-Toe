d62625ec8dc85bb4d619806d48e059239ffcab05
AllanT102 <allan.zf.tan@gmail.com>
2022-07-25 01:32:51 -0700

    Completed game-board

---
+++ app.js
@@ -0,0 +1,14 @@
+// gameboard
+
+const gameboard = (function () {
+
+    return {}
+})
+
+//player
+
+// const test1 = document.querySelector('.grid-box1')
+// test1.addEventListener('mouseover', () => {
+//     test1.style.backgroundColor = "red"
+// })
+
